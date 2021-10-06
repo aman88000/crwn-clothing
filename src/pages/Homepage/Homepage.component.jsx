e53393f54f2d43e7f809e083d11257accb0cc356
aman88000 <aman88000@gmail.com>
2021-10-06 23:48:31 +0530

    create new component and add some style

---
+++ src/pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.component'
+import './Homepage.style.scss'
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
